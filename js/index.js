3a14a109b5ed250b19d06ab3bc72121cdd09b641
Jamal Combs <jamal.m.combs@gmail.com>
2018-03-21 00:20:46 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,42 @@
 (function($) {
     "use strict"; // Start of use strict
-
+    
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
+    $('a.page-scroll')
+        .bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body')
+                .stop()
+                .animate({
+                    scrollTop: ($($anchor.attr('href'))
+                        .offset()
+                        .top - 50)
+                }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
+    $('body')
+        .scrollspy({
+            target: '.navbar-fixed-top',
+            offset: 51
+        });
+    
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
+    $('.navbar-collapse ul li a')
+        .click(function() {
+            $('.navbar-toggle:visible')
+                .click();
+        });
+    
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
+    $('#mainNav')
+        .affix({
+            offset: {
+                top: 100
+            }
+        })
+    
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
@@ -44,21 +53,21 @@
         scale: 0.3,
         distance: '0px'
     }, 300);
-
+    
     // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
-
+    $('.popup-gallery')
+        .magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            }
+        });
 })(jQuery); // End of use strict
